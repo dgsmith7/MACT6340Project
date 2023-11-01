982f318ea4fed50caddb44c3ee583365c3dfba94
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-10-31 19:50:02 -0500

    added bootstrap and validation to contact form

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,17 +1,30 @@
 (function () {
   "use strict";
 
-  document
-    .querySelector("#contact-form-button")
-    .addEventListener("click", (event) => {
-      event.preventDefault;
-      event.stopPropagation;
-      console.log("You clicked the submit button.");
-      let name = document.querySelector("#name").value;
-      let email = document.querySelector("#mail").value;
-      let message = document.querySelector("#msg").value;
-      console.log("Name: " + name);
-      console.log("Email: " + email);
-      console.log("Message: " + message);
-    });
+  let form = document.querySelector("#contact-form");
+
+  document.querySelector("#send-contact").addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    let formValid = true;
+    if (!form.checkValidity()) {
+      formValid = false;
+    }
+    form.classList.add("was-validated");
+    if (formValid) {
+      sendTheEmail();
+    }
+  });
+
+  function sendTheEmail() {
+    console.log("You clicked the submit button.");
+    let firstName = document.querySelector("#contact-first").value;
+    let lastName = document.querySelector("#contact-first").value;
+    let email = document.querySelector("#contact-email-addr").value;
+    let message = document.querySelector("#contact-question").value;
+    console.log("First name: " + firstName);
+    console.log("Last name: " + lastName);
+    console.log("Email: " + email);
+    console.log("Message: " + message);
+  }
 })();
