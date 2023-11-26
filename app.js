215615069b16a0f060b9997c3067f6aaef28f39e
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-11-26 17:15:44 -0600

    added EJS.  Made header footer templates.  Fixed individual proejct endpoints.  Added error handling.

--- app.js
+++ app.js
@@ -2,15 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs", { projectArray: data });
+});
+
+app.get("/project/:id", (req, res) => {
+  let id = req.params.id;
+  if (id > data.length) {
+    throw new Error("No project with that ID");
+  }
+  res.render("project.ejs", { projectArray: data, which: id });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 
 app.post("/mail", async (req, res) => {
   await utils
@@ -21,6 +39,17 @@ app.post("/mail", async (req, res) => {
     .catch(() => {
       res.send({ result: "failure" });
     });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  let msg;
+  msg = err.message;
+  if (msg != "No project with that ID") {
+    msg =
+      "There was an internal error. Apologies. We are working on cleaning up the mess.";
+  }
+  res.render("error.ejs", { msg: msg });
 });
 
 app.listen(port, () => {
